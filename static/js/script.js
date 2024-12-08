e178d21c86a5f2cd242372d0d30df58cb63c6c2c
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-08 01:42:56 -0300

    fix: navbar switch theme bug fixed

--- static/js/script.js
+++ static/js/script.js
@@ -6,18 +6,26 @@ menuToggle.addEventListener('click', () => {
 });
 
 const switchTheme = document.getElementById('switchTheme');
-const buttonTheme = document.getElementById('buttonTheme');
-buttonTheme.addEventListener('click', () => {
-    if (switchTheme.classList.contains('bg-zinc-900')){
-        
+
+// Selecione os dois botões "Trocar Tema" com os novos IDs
+const buttonThemeDesktop = document.getElementById('buttonThemeDesktop');
+const buttonThemeMobile = document.getElementById('buttonThemeMobile');
+
+// Função para alternar os temas
+const toggleTheme = () => {
+    if (switchTheme.classList.contains('bg-zinc-900')) {
         switchTheme.classList.remove('bg-zinc-900');
         switchTheme.classList.remove('text-white');
         switchTheme.classList.add('bg-gray-100');
         switchTheme.classList.add('text-black');
         return;
-    }
+    } 
     switchTheme.classList.remove('bg-gray-100');
     switchTheme.classList.remove('text-black');
     switchTheme.classList.add('bg-zinc-900');
     switchTheme.classList.add('text-white');
-})
+    
+}
+
+buttonThemeDesktop.addEventListener('click', toggleTheme);
+buttonThemeMobile.addEventListener('click', toggleTheme);
