925480333ff6314197d36efb3cfffc3b2f2ee941
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-08 00:56:03 -0300

    feat: footer added

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-app_name = 'project'
+app_name = 'appointments'
 
 
 def test_view(request):
-    return render(request, 'base.html')
+    return render(request, 'appointments/index.html')
--- static/js/script.js
+++ static/js/script.js
@@ -9,10 +9,15 @@ const switchTheme = document.getElementById('switchTheme');
 const buttonTheme = document.getElementById('buttonTheme');
 buttonTheme.addEventListener('click', () => {
     if (switchTheme.classList.contains('bg-zinc-900')){
+        
         switchTheme.classList.remove('bg-zinc-900');
+        switchTheme.classList.remove('text-white');
         switchTheme.classList.add('bg-gray-100');
+        switchTheme.classList.add('text-black');
         return;
     }
     switchTheme.classList.remove('bg-gray-100');
+    switchTheme.classList.remove('text-black');
     switchTheme.classList.add('bg-zinc-900');
+    switchTheme.classList.add('text-white');
 })
