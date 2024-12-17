ef666e5ed125b19f8c726d6ef96748b44dbded2d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-17 12:45:51 -0300

    feat: 404 page has been created

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.shortcuts import render
 from django.conf.urls import handler404
 from appointments.pages.barber_views import ServiceViewSet
 from appointments.pages.scheduling_views import ScheduleViewSet
@@ -24,7 +23,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.defaults import page_not_found
 
 handler404 = custom_404_view
 
@@ -37,8 +35,8 @@ urlpatterns = [
     path('', include('appointments.urls')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('404/', lambda request: page_not_found(request, exception=None))
 ]
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
