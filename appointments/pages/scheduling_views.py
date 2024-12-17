b135f3a8c555bda944901070eda45289130585cb
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-17 00:29:43 -0300

    feat(appointments): add provisional 404 page and UpdateScheduleView

--- appointments/pages/scheduling_views.py
+++ appointments/pages/scheduling_views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 import os
 import requests
 from rest_framework import viewsets
@@ -5,10 +6,14 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.views.generic import View, ListView
+from django.db import transaction
+from django.utils import timezone
 from ..forms.scheduling_forms import ScheduleForm
 from ..utils.others import get_env
 from ..models import Scheduling
 from ..serializers import ScheduleSerializer
+from ..services.google_calendar_service import (
+    insert_into_calendar, delete_from_calendar)
 get_env()
 
 
@@ -74,6 +79,72 @@ class ReadSchedulingView(LoginRequiredMixin, ListView):
         self.object_list = self.get_queryset()
         context = self.get_context_data(object_list=self.object_list)
         return self.render_to_response(context)
+
+
+class UpdateScheduleView(LoginRequiredMixin, View):
+    template_name = 'appointments/update_schedule.html'
+
+    def get(self, request, schedule_id):
+
+        existing_schedule = get_object_or_404(Scheduling, pk=schedule_id)
+
+        if existing_schedule.client != request.user:
+            return render(request, '404.html', status=404)
+
+        initial_data = {
+            'service': existing_schedule.service,
+            'date_time': existing_schedule.date_time.strftime(
+                '%Y-%m-%dT%H:%M'),
+            'notes': existing_schedule.notes,
+        }
+        form = ScheduleForm(initial=initial_data)
+        return render(request, self.template_name,
+                      {'form': form, 'schedule_id': schedule_id})
+
+    def post(self, request, schedule_id):
+        existing_schedule = get_object_or_404(Scheduling, pk=schedule_id)
+
+        form = ScheduleForm(request.POST, instance=existing_schedule)
+
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    if existing_schedule.calendar_event_id:
+                        delete_from_calendar(
+                            existing_schedule.calendar_event_id)
+
+                    scheduling = form.save(commit=False)
+                    scheduling.client = request.user
+                    scheduling.save()
+
+                    start_time = scheduling.date_time
+                    end_time = start_time + \
+                        timezone.timedelta(minutes=scheduling.service.duration)
+
+                    created_event = insert_into_calendar(
+                        summary=scheduling.service.service_name,
+                        start=start_time,
+                        end=end_time,
+                        description=form.cleaned_data.get('notes', '')
+                    )
+
+                    if not created_event:
+                        raise Exception(
+                            'Erro ao inserir o agendamento na Agenda.')
+
+                    scheduling.calendar_event_id = created_event['id']
+                    scheduling.save()
+
+                messages.success(
+                    request, 'Agendamento atualizado com sucesso.')
+
+                return redirect('appointments:schedules')
+
+            except Exception as e:
+                messages.error(request, f'Ocorreu um erro: {e}')
+
+        return render(request, self.template_name,
+                      {'form': form, 'schedule_id': schedule_id})
 
 
 class ScheduleViewSet(viewsets.ModelViewSet):
