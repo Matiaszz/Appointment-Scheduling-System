f60620333b3f5c7156415595bb15d9bb1c0ae521
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-20 14:45:12 -0300

    feat: the forget password button added, change password button and forgett passowrd button working

--- project/urls.py
+++ project/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.shortcuts import render
-from django.conf.urls import handler404, handler403
 from appointments.pages.barber_views import ServiceViewSet
 from appointments.pages.scheduling_views import ScheduleViewSet
 from appointments.views import custom_403_view, custom_404_view
 from rest_framework.routers import DefaultRouter
+from django.shortcuts import render
+from django.conf.urls import handler404, handler403
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 handler403 = custom_403_view
 handler404 = custom_404_view
@@ -39,6 +40,25 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('contact/', lambda request: render(request,
          'contact_us.html'), name='contact'),
+
+    # Password views
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+
+
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
 
 if settings.DEBUG:
